79222e320030db6f49a2f0f13607f99d3db73c5a
Lyetus <Sz80top80@yandex.ru>
2019-11-14 16:39:18 +0300

    Update newjavascript.js

--- newjavascript.js
+++ newjavascript.js
@@ -1,8 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 $(function() {
  
     $('#button').on("click", function() {
@@ -40,7 +35,7 @@ $(function() {
     });
     
     $('#pngA').on("click", function(){
-        alert("Теперь на GitHub!!!");
+        alert("Комитов нет, но вы держитесь...");
     });
     
     $('#button2').on("click", function(){
